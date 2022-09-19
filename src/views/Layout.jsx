1dd7943c46feea4715da47e3ea09c6f22c10447e
Alexey Zhukov <alexpaef@gmail.com>
2022-09-19 14:50:33 +0300

    Fixed eslint

--- src/views/Layout.jsx
+++ src/views/Layout.jsx
@@ -1,54 +1,55 @@
 const React = require('react');
 
+// eslint-disable-next-line react/prop-types
 module.exports = function Layout({ children }) {
   return (
-    <html lang='en'>
+    <html lang="en">
       <head>
-        <meta charSet='UTF-8' />
-        <meta httpEquiv='X-UA-Compatible' content='IE=edge' />
-        <meta name='viewport' content='width=device-width, initial-scale=1.0' />
+        <meta charSet="UTF-8" />
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>Document</title>
         <link
-          rel='stylesheet'
-          href='https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css'
-          integrity='sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx'
-          crossOrigin='anonymous'
-        ></link>
-        <link rel='stylesheet' href='/style/main.css' />
+          rel="stylesheet"
+          href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css"
+          integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx"
+          crossOrigin="anonymous"
+        />
+        <link rel="stylesheet" href="/style/main.css" />
         <script
           defer
-          src='https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.min.js'
-          integrity='sha384-7VPbUDkoPSGFnVtYi0QogXtr74QeVeeIs99Qfg5YCF+TidwNdjvaKZX19NZ/e6oz'
-          crossOrigin='anonymous'
-        ></script>
-        <script src='/js/client.js'></script>
+          src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.min.js"
+          integrity="sha384-7VPbUDkoPSGFnVtYi0QogXtr74QeVeeIs99Qfg5YCF+TidwNdjvaKZX19NZ/e6oz"
+          crossOrigin="anonymous"
+        />
+        <script src="/js/client.js" />
       </head>
       <body>
-        <nav className='navbar navbar-expand-lg bg-light'>
-          <div className='container-fluid'>
-            <a className='navbar-brand' href='/'>
+        <nav className="navbar navbar-expand-lg bg-light">
+          <div className="container-fluid">
+            <a className="navbar-brand" href="/">
               Todos
             </a>
             <button
-              className='navbar-toggler'
-              type='button'
-              data-bs-toggle='collapse'
-              data-bs-target='#navbarNav'
-              aria-controls='navbarNav'
-              aria-expanded='false'
-              aria-label='Toggle navigation'
+              className="navbar-toggler"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#navbarNav"
+              aria-controls="navbarNav"
+              aria-expanded="false"
+              aria-label="Toggle navigation"
             >
-              <span className='navbar-toggler-icon'></span>
+              <span className="navbar-toggler-icon" />
             </button>
-            <div className='collapse navbar-collapse' id='navbarNav'>
-              <ul className='navbar-nav'>
-                <li className='nav-item'>
-                  <a className='nav-link' href='/todos'>
+            <div className="collapse navbar-collapse" id="navbarNav">
+              <ul className="navbar-nav">
+                <li className="nav-item">
+                  <a className="nav-link" href="/todos">
                     Todos
                   </a>
                 </li>
-                <li className='nav-item'>
-                  <a className='nav-link' href='/private'>
+                <li className="nav-item">
+                  <a className="nav-link" href="/private">
                     Private
                   </a>
                 </li>
@@ -56,8 +57,8 @@ module.exports = function Layout({ children }) {
             </div>
           </div>
         </nav>
-        <div className='container layout'>{children}</div>
-        <footer className='footer'>
+        <div className="container layout">{children}</div>
+        <footer className="footer">
           <div>Bees 2022</div>
         </footer>
       </body>
